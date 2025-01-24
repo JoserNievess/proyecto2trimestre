c9435598cf0d750fbed8d2f37ad72bd282ab091f
JoserNievess <joseraimundo.ma40939@cesurformacion.com>
2025-01-24 10:03:06 +0100

    pagina posts 11

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,14 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('/blog/{post:slug}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/', HomeController::class)->name('home');
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
 });
